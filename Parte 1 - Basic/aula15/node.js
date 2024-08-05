63e9bd34acabc9a9c70ae3fe9f2ce77427db8838
Minerva <superetro85@gmail.com>
2024-08-05 20:04:27 -0300

    dAY2

--- Parte 1 - Basic/aula15/node.js
+++ Parte 1 - Basic/aula15/node.js
@@ -1,2 +1,17 @@
-const numero = prompt('digite um numero')
-const numeroTitulo = document.getElementById('numero-titulo')
+const numero = prompt('Digite um numero: ')
+const nummeroTitulo = document.getElementById('numero-titulo')
+
+nummeroTitulo.innerHTML = numero
+
+const raiz = document.getElementById('raiz')
+raiz = numero ** 0.5
+
+nummeroTitulo.innerHTML = raiz
+
+
+
+
+
+// Estudei mais sobre banco e alguns exercicios
+// E javascript ESTUDANDO
+// A Sabrina me apresentou o Sistema de Armas e um pouco sobre o banco
