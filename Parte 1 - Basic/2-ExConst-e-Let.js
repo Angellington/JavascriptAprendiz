a728628a583c350b9df5413c86b02c73245cce9f
Minerva <superetro85@gmail.com>
2024-08-07 20:17:13 -0300

    Array e Functions

--- Parte 1 - Basic/2-ExConst-e-Let.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")()
-
-console.clear()
-let nome = 'Wellington'
-let sobrenome = 'Ferreira'
-let idade = 19
-
-
-console.log(nome, sobrenome, idade)
--- Parte 1 - Basic/aula15/node.js
+++ Parte 1 - Basic/aula15/node.js
@@ -1,17 +1,12 @@
-const numero = prompt('Digite um numero: ')
-const nummeroTitulo = document.getElementById('numero-titulo')
+let numero = Number(prompt("Digite um numero: "))
 
-nummeroTitulo.innerHTML = numero
+texto = document.getElementById('num')
 
-const raiz = document.getElementById('raiz')
-raiz = numero ** 0.5
-
-nummeroTitulo.innerHTML = raiz
-
-
-
+texto.innerHTML = numero
+texto.innerHTML += `<p>Raiz Quadrada ${numero ** 0.5}</p>`
+texto.innerHTML += `<p>O ${numero} é inteiro: ${Number.isInteger(numero)}</p>`
+texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`
+texto.innerHTML += `<p>Arredondando para cima: ${Math.ceil(numero)}</p>`
+texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`
 
 
-// Estudei mais sobre banco e alguns exercicios
-// E javascript ESTUDANDO
-// A Sabrina me apresentou o Sistema de Armas e um pouco sobre o banco
