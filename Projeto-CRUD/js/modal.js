b7ebfa380641032aa621c18e470f75d27f11fd64
Minerva <superetro85@gmail.com>
2024-08-23 14:32:46 -0300

    mobile: responsive

--- Projeto-CRUD/js/modal.js
+++ Projeto-CRUD/js/modal.js
@@ -1,4 +1,5 @@
 const abrirDialog = document.querySelector(".create-contain")
+const FloatButtonOpen = document.querySelector(".floating-button")
 
 // const fecharDialog = document.querySelector("#closeIcon")
 
@@ -8,6 +9,10 @@ const modal = document.querySelector("dialog")
 
 
 abrirDialog.onclick = function() {
+    modal.showModal()
+}
+// Vai abrir o dialog com o Float Button
+FloatButtonOpen.onclick = function() {
     modal.showModal()
 }
 
