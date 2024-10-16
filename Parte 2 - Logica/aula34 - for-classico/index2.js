2a6d32645140e13a7ddd94496f976b16efe25c4a
Minerva <superetro85@gmail.com>
2024-10-16 11:41:06 -0500

    For Clássico

---
+++ Parte 2 - Logica/aula34 - for-classico/index2.js
@@ -0,0 +1,6 @@
+console.clear()
+const color = require('colors')
+//   (Variavel(i))  (condicao)  (incrementar)
+for(   let i = 0;    i <= 10000000;     i += 100){
+    console.log(`Linha ${i}`.rainbow)
+}
