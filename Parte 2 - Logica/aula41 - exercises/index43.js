96c39ac05fb9a60dd496e08bb0f084620860e61d
Seu Nome de Usuário <superetro85@gmail.com>
2024-11-11 16:39:59 -0500

    Add: Last exercises

--- Parte 2 - Logica/aula41 - exercises/index43.js
+++ Parte 2 - Logica/aula41 - exercises/index43.js
@@ -12,10 +12,36 @@ let n = prompt("Tab a number: ");
 
 // parte 1
 
-function Parte1(n){
-    if(n % 3 == 0){
-        console.log("Fizz");
+function Parte3(n){
+    if(n % 3 == 0 && n % 5 == 0){
+        console.log("FizzBuzz")
+    }     
+    else {
+        return n
     }
 }
 
-Parte1(n)
+n = Parte3(n)
+
+
+
+function Parte1(n){
+    if(n % 3 == 0){
+        console.log("Fizz");
+    } else {
+        return n
+    }
+}
+
+
+n = Parte1(n);
+
+function Parte2(n){
+    if(n % 5 == 0){
+        console.log("Buzz")
+    } else {
+        console.log(n)
+    }
+}
+
+n = Parte2(n)
