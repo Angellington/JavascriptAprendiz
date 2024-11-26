5b9e4ebd1b0ed62a3a226a193eddd0eb4105db99
Seu Nome de Usuário <superetro85@gmail.com>
2024-11-26 18:03:00 -0500

    String Trainings

---
+++ Parte 2 - Logica/revisao/strings/another.js
@@ -0,0 +1,11 @@
+console.clear()
+
+// let text = "Minerva"
+// let text2 = "Alves"
+
+
+
+// console.log(text.concat(" ", text2));
+
+let name = "            Minerva Alves Barbosa                         "
+console.log(name.trimEnd());
---
+++ Parte 2 - Logica/revisao/strings/substring.js
@@ -0,0 +1,3 @@
+console.clear()
+let str = "Apple, Banana, Kiwi";
+console.log(str.substring(4, 20))
